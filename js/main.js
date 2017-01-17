27f2bd712e58437a167ddd8f6fefb74ba097d411
dmitrymorozoff <dimemorozovy@yandex.ru>
2017-01-17 23:13:58 +0300

    create card, change style

--- js/main.js
+++ js/main.js
@@ -45,6 +45,7 @@ function stopDragging(e) {
 /***Работа с модальным окном***/
 var modal = document.getElementsByClassName('modal')[0];
 var closeModalButton = document.getElementsByClassName('close-modal')[0];
+var closeCardButton = document.getElementsByClassName('close-card')[0];
 
 function createModalContent(modal, element) {
     var div = document.createElement('div');
@@ -53,9 +54,43 @@ function createModalContent(modal, element) {
     modal.appendChild(div);
 }
 
+function createModalNewCard(modal, targetUl) {
+    var spanTitle = document.createElement('span');
+    spanTitle.className = 'modal-label';
+    spanTitle.innerText = 'Title';
+    var inputTitle = document.createElement('input');
+    inputTitle.type = 'text';
+    inputTitle.classList.add('modal-input', 'modal-input-title');
+    var spanDesc = document.createElement('span');
+    spanDesc.className = 'modal-label';
+    spanDesc.innerText = 'Description';
+    var textarea = document.createElement('textarea');
+    textarea.name = 'modal-desc';
+    textarea.classList.add('modal-textarea', 'modal-textarea-desc');
+    var addButton = document.createElement('a');
+    addButton.href = '#';
+    addButton.classList.add('add-task-button', 'modal-create-button');
+    addButton.innerText = 'Create new card';
+    modal.appendChild(spanTitle);
+    modal.appendChild(inputTitle);
+    modal.appendChild(spanDesc);
+    modal.appendChild(textarea);
+    modal.appendChild(addButton);
+    addButton.onclick = function(event) {
+        event.preventDefault();
+        var newCard = createNewCard('item', 'New card with new description!', targetUl);
+        newCard.addEventListener('dragstart', startDragging, false);
+        newCard.addEventListener('dragend', stopDragging, false);
+        newCard.addEventListener('click', onClickCard, false);
+        newCard.addEventListener('mouseover', showCloseButtonAndRemoveCard, false);
+        newCard.addEventListener('mouseout', hideCloseButton, false);
+        targetUi.appendChild(newCard);
+    };
+}
+
 function onClickCard(event) {
     if (!modal.classList.contains('active')) {
-        if (event.target.tagName !== 'LABEL' && event.target.tagName !== 'INPUT') {
+        if (event.target.tagName !== 'LABEL' && event.target.tagName !== 'INPUT' && event.target.tagName !== 'I') {
             modal.classList.add('active');
             createModalContent(modal.getElementsByClassName('modal-content')[0], this);
         }
@@ -64,14 +99,28 @@ function onClickCard(event) {
 closeModalButton.addEventListener('click', function() {
     modal.classList.remove('active');
     var content = modal.getElementsByClassName('modal-content')[0];
-    console.log(content);
     content.innerHTML = '';
 });
 
+function showCloseButtonAndRemoveCard() {
+    var closeButton = this.getElementsByClassName('close-card')[0];
+    closeButton.style.opacity = '1';
+    closeButton.onclick = function(event) {
+        var currentCard = this.parentElement;
+        var ul = currentCard.parentElement;
+        ul.removeChild(currentCard);
+    };
+}
+
+function hideCloseButton() {
+    this.getElementsByClassName('close-card')[0].style.opacity = '0';
+}
 var cards = document.querySelectorAll('.card');
 [].forEach.call(cards, function(card) {
     card.addEventListener('dragstart', startDragging, false);
     card.addEventListener('dragend', stopDragging, false);
+    card.addEventListener('mouseover', showCloseButtonAndRemoveCard, false);
+    card.addEventListener('mouseout', hideCloseButton, false);
     card.addEventListener('click', onClickCard, false);
 });
 var columns = document.querySelectorAll('.column');
@@ -82,15 +131,19 @@ var columns = document.querySelectorAll('.column');
     column.addEventListener('drop', drop, false);
 });
 /***Добавление новой карточки***/
-function createNewCard(title, desc) {
+function createNewCard(title, desc, targetUl) {
     var li = document.createElement('li');
     li.className = 'card';
     li.setAttribute("draggable", true);
     var p = document.createElement('p');
     p.className = 'card-desc';
     p.innerText = desc;
+    var i = document.createElement('i');
+    i.classList.add('ionicons', 'ion-android-close', 'close-card');
+    var input = document.createElement('i');
     li.appendChild(p);
-    return li;
+    li.appendChild(i);
+    targetUl.appendChild(li);
     // var descElem = getChildrenElementByClassName(element, 'card-desc');
     // div.className = "alert alert-success";
     // div.innerHTML = descElem.innerHTML;
@@ -101,12 +154,14 @@ function onClickNewCardButton(e) {
     //получили все данные необходимые для вставки нового элемента
     //.....
     //создаем новую карточку с полученными параметрами
-    var newCard = createNewCard('item', 'New card with new description!');
-    newCard.addEventListener('dragstart', startDragging, false);
-    newCard.addEventListener('dragend', stopDragging, false);
-    newCard.addEventListener('click', onClickCard, false);
     var targetUl = this.parentElement.getElementsByClassName('card-list')[0];
-    targetUl.appendChild(newCard);
+    if (!modal.classList.contains('active')) {
+        modal.classList.add('active');
+        createModalNewCard(modal.getElementsByClassName('modal-content')[0], targetUl);
+    }
+
+
+    //targetUl.appendChild(newCard);
 }
 var newCardButtons = document.getElementsByClassName('add-task-button');
 [].forEach.call(newCardButtons, function(newCardButton) {
