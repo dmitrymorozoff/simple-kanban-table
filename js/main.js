13db11a0e14324028f00446ffdca01ea502ab4c0
dmitrymorozoff <dimemorozovy@yandex.ru>
2017-01-23 11:51:30 +0300

    create new cards, sidebar and more

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+//Полифилл append для IE
+(function (arr) {
+    arr.forEach(function (item) {
+        item.append = item.append || function () {
+            var argArr = Array.prototype.slice.call(arguments),
+                docFrag = document.createDocumentFragment();
+            argArr.forEach(function (argItem) {
+                var isNode = argItem instanceof Node;
+                docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+            });
+            this.appendChild(docFrag);
+        };
+    });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
 var movingCard = null;
-/***Обработка событий Drag and Drop***/
+//Обработка событий Drag and Drop
 function startDragging(e) {
     this.classList.add('dragging');
     movingCard = this;
@@ -36,24 +51,86 @@ function drop(e) {
 }
 
 function stopDragging(e) {
-    [].forEach.call(columns, function(column) {
+    [].forEach.call(columns, function (column) {
         column.classList.remove('over');
     });
     movingCard.classList.remove('dragging');
     movingCard = null;
 }
-/***Работа с модальным окном***/
+
 var modal = document.getElementsByClassName('modal')[0];
 var closeModalButton = document.getElementsByClassName('close-modal')[0];
 var closeCardButton = document.getElementsByClassName('close-card')[0];
-
+//Добавляем подробную информацию о карточке в модальное окно
 function createModalContent(modal, element) {
-    var div = document.createElement('div');
-    var descElem = element.getElementsByClassName('card-desc')[0];
-    div.innerHTML = descElem.innerHTML;
-    modal.appendChild(div);
+    if (element.getElementsByClassName('card-photo-item')[0]) {
+        var photoWrapper = document.createElement('div');
+        photoWrapper.className = 'modal-photo';
+        var photo = document.createElement('img');
+        photo.className = 'modal-photo-item';
+        var photoFromCard = element.getElementsByClassName('card-photo-item')[0];
+        photo.setAttribute('src', photoFromCard.getAttribute('src'));
+        photoWrapper.appendChild(photo);
+        modal.appendChild(photoWrapper);
+    }
+    if (element.getElementsByClassName('card-title')[0]) {
+        var h3 = document.createElement('h3');
+        h3.className = 'title-modal-content';
+        var titleElem = element.getElementsByClassName('card-title')[0];
+        h3.innerHTML = titleElem.innerText;
+        modal.appendChild(h3);
+    }
+    if (element.getElementsByClassName('card-desc')[0]) {
+        var p = document.createElement('p');
+        p.className = 'desc-modal-content';
+        var descElem = element.getElementsByClassName('card-desc')[0];
+        p.innerHTML = descElem.innerText;
+        modal.appendChild(p);
+    }
+}
+//Массив объектов с информацией о радио-кнопках
+var priorities = [{
+    title: 'Low',
+    dataAttr: 'low'
+}, {
+    title: 'Medium',
+    dataAttr: 'medium'
+}, {
+    title: 'High',
+    dataAttr: 'high'
+}];
+//Создание радио-кнопок
+function createRadioGroup(items) {
+    var ul = document.createElement('ul');
+    ul.className = 'radio-list';
+    for (var i = 0; i < items.length; i++) {
+        var li = document.createElement('li');
+        li.className = 'radio-list-item';
+        var label = document.createElement('label');
+        label.className = 'label-radio';
+        var input = document.createElement('input');
+        input.type = 'radio';
+        input.className = 'radio';
+        input.name = 'foo';
+        input.setAttribute('data-priority', items[i].dataAttr);
+        label.appendChild(input);
+        label.append(items[i].title);
+        li.appendChild(label);
+        ul.appendChild(li);
+    }
+    return ul;
 }
 
+function getCheckedRadio(radio_group) {
+    for (var i = 0; i < radio_group.length; i++) {
+        var button = radio_group[i];
+        if (button.checked) {
+            return button;
+        }
+    }
+    return undefined;
+}
+//Модальное окно с созданием новой карточки
 function createModalNewCard(modal, targetUl) {
     var spanTitle = document.createElement('span');
     spanTitle.className = 'modal-label';
@@ -71,23 +148,36 @@ function createModalNewCard(modal, targetUl) {
     addButton.href = '#';
     addButton.classList.add('add-task-button', 'modal-create-button');
     addButton.innerText = 'Create new card';
+    var spanPriority = document.createElement('span');
+    spanPriority.className = 'modal-label';
+    spanPriority.innerText = 'Priority';
+    var radio = createRadioGroup(priorities);
+    modal.appendChild(spanPriority);
+    modal.appendChild(radio);
     modal.appendChild(spanTitle);
     modal.appendChild(inputTitle);
     modal.appendChild(spanDesc);
     modal.appendChild(textarea);
     modal.appendChild(addButton);
-    addButton.onclick = function(event) {
+    var currentCheckedRadio = null;
+    radio.onchange = function (event) {
+        currentCheckedRadio = event.target.getAttribute('data-priority');
+    };
+    addButton.onclick = function (event) {
         event.preventDefault();
-        var newCard = createNewCard('item', 'New card with new description!', targetUl);
+        var newCard = createNewCard(modal.getElementsByClassName('modal-input-title')[0].value, modal.getElementsByClassName('modal-textarea-desc')[0].value, currentCheckedRadio, targetUl);
         newCard.addEventListener('dragstart', startDragging, false);
         newCard.addEventListener('dragend', stopDragging, false);
         newCard.addEventListener('click', onClickCard, false);
-        newCard.addEventListener('mouseover', showCloseButtonAndRemoveCard, false);
+        newCard.addEventListener('mouseover', removeCard, false);
         newCard.addEventListener('mouseout', hideCloseButton, false);
-        targetUi.appendChild(newCard);
+        targetUl.appendChild(newCard);
+        this.parentElement.parentElement.classList.remove('active');
+        var content = this.parentElement.parentElement.getElementsByClassName('modal-content')[0];
+        content.innerHTML = '';
     };
 }
-
+//Обработка клика на карточку
 function onClickCard(event) {
     if (!modal.classList.contains('active')) {
         if (event.target.tagName !== 'LABEL' && event.target.tagName !== 'INPUT' && event.target.tagName !== 'I') {
@@ -96,74 +186,118 @@ function onClickCard(event) {
         }
     }
 }
-closeModalButton.addEventListener('click', function() {
+//Закрываем модальное окно с подробной информацией о карточке
+closeModalButton.addEventListener('click', function () {
     modal.classList.remove('active');
     var content = modal.getElementsByClassName('modal-content')[0];
     content.innerHTML = '';
 });
-
-function showCloseButtonAndRemoveCard() {
+//Удаление карточки
+function removeCard() {
     var closeButton = this.getElementsByClassName('close-card')[0];
     closeButton.style.opacity = '1';
-    closeButton.onclick = function(event) {
-        var currentCard = this.parentElement;
-        var ul = currentCard.parentElement;
-        ul.removeChild(currentCard);
+    closeButton.onclick = function (event) {
+        var that = this;
+        var alert = createAlert('Are you sure?', 'Are you sure that you want to delete this card?');
+        alert.onclick = function (event) {
+            if (event.target.classList.contains('alert-button-ok')) {
+                this.parentNode.removeChild(this);
+                var currentCard = that.parentElement;
+                var ul = currentCard.parentElement;
+                ul.removeChild(currentCard);
+            }
+            if (event.target.classList.contains('alert-button-no')) {
+                this.parentNode.removeChild(this);
+            }
+        };
     };
 }
-
+//Прячем кнопку удаления
 function hideCloseButton() {
     this.getElementsByClassName('close-card')[0].style.opacity = '0';
 }
+//Вешаем обработчики событий на карточки и колонки
 var cards = document.querySelectorAll('.card');
-[].forEach.call(cards, function(card) {
+[].forEach.call(cards, function (card) {
     card.addEventListener('dragstart', startDragging, false);
     card.addEventListener('dragend', stopDragging, false);
-    card.addEventListener('mouseover', showCloseButtonAndRemoveCard, false);
+    card.addEventListener('mouseover', removeCard, false);
     card.addEventListener('mouseout', hideCloseButton, false);
     card.addEventListener('click', onClickCard, false);
 });
 var columns = document.querySelectorAll('.column');
-[].forEach.call(columns, function(column) {
+[].forEach.call(columns, function (column) {
     column.addEventListener('dragenter', dragInto, false);
     column.addEventListener('dragover', dragWithin, false);
     column.addEventListener('dragleave', dragOut, false);
     column.addEventListener('drop', drop, false);
 });
-/***Добавление новой карточки***/
-function createNewCard(title, desc, targetUl) {
+//Добавление новой карточки
+function createNewCard(title, desc, priority, targetUl) {
     var li = document.createElement('li');
     li.className = 'card';
     li.setAttribute("draggable", true);
+    li.setAttribute('data-priority', priority);
+    var cardPhoto = document.createElement('div');
+    cardPhoto.className = 'card-photo';
+    var cardPhotoItem = document.createElement('img');
+    cardPhotoItem.className = 'card-photo-item';
+    cardPhotoItem.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample1.jpg';
+    cardPhotoItem.alt = 'ava';
+    cardPhoto.appendChild(cardPhotoItem);
+    var cardInfo = document.createElement('div');
+    cardInfo.className = 'card-info';
+    var h3 = document.createElement('h3');
+    h3.className = 'card-title';
+    h3.innerText = title;
     var p = document.createElement('p');
     p.className = 'card-desc';
     p.innerText = desc;
+    cardInfo.appendChild(h3);
+    cardInfo.appendChild(p);
     var i = document.createElement('i');
     i.classList.add('ionicons', 'ion-android-close', 'close-card');
     var input = document.createElement('i');
-    li.appendChild(p);
+    li.appendChild(cardPhoto);
+    li.appendChild(cardInfo);
     li.appendChild(i);
     targetUl.appendChild(li);
-    // var descElem = getChildrenElementByClassName(element, 'card-desc');
-    // div.className = "alert alert-success";
-    // div.innerHTML = descElem.innerHTML;
+    return li;
 }
-
+//Обработка клика создания новой карточки
 function onClickNewCardButton(e) {
     e.preventDefault();
-    //получили все данные необходимые для вставки нового элемента
-    //.....
-    //создаем новую карточку с полученными параметрами
     var targetUl = this.parentElement.getElementsByClassName('card-list')[0];
     if (!modal.classList.contains('active')) {
         modal.classList.add('active');
         createModalNewCard(modal.getElementsByClassName('modal-content')[0], targetUl);
     }
-
-
-    //targetUl.appendChild(newCard);
 }
+//Вешаем событие на кнопку добавление карточки
 var newCardButtons = document.getElementsByClassName('add-task-button');
-[].forEach.call(newCardButtons, function(newCardButton) {
+[].forEach.call(newCardButtons, function (newCardButton) {
     newCardButton.addEventListener('click', onClickNewCardButton, false);
 });
+//Создание уведомлений
+function createAlert(title, description) {
+    var alert = document.createElement('div');
+    alert.className = 'alert';
+    var alertTitle = document.createElement('p');
+    alertTitle.className = 'alert-title';
+    alertTitle.innerText = title;
+    var alertDesc = document.createElement('p');
+    alertDesc.className = 'alert-desc';
+    alertDesc.innerText = description;
+    var alertButtonYes = document.createElement('a');
+    alertButtonYes.classList.add('alert-button', 'alert-button-ok');
+    alertButtonYes.innerText = 'Yes';
+    var alertButtonNo = document.createElement('a');
+    alertButtonNo.classList.add('alert-button', 'alert-button-no');
+    alertButtonNo.innerText = 'No';
+    alert.appendChild(alertTitle);
+    alert.appendChild(alertDesc);
+    alert.appendChild(alertButtonYes);
+    alert.appendChild(alertButtonNo);
+    document.body.appendChild(alert);
+    return alert;
+}
