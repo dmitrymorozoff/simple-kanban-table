67e1ff4fc1f39c573b8ec9341165f51d3f98ba74
dmitrymorozoff <dimemorozovy@yandex.ru>
2017-01-24 16:09:00 +0300

    add edit function and more...

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 //Полифилл append для IE
-(function (arr) {
-    arr.forEach(function (item) {
-        item.append = item.append || function () {
+(function(arr) {
+    arr.forEach(function(item) {
+        item.append = item.append || function() {
             var argArr = Array.prototype.slice.call(arguments),
                 docFrag = document.createDocumentFragment();
-            argArr.forEach(function (argItem) {
+            argArr.forEach(function(argItem) {
                 var isNode = argItem instanceof Node;
                 docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
             });
@@ -51,7 +51,7 @@ function drop(e) {
 }
 
 function stopDragging(e) {
-    [].forEach.call(columns, function (column) {
+    [].forEach.call(columns, function(column) {
         column.classList.remove('over');
     });
     movingCard.classList.remove('dragging');
@@ -60,7 +60,7 @@ function stopDragging(e) {
 
 var modal = document.getElementsByClassName('modal')[0];
 var closeModalButton = document.getElementsByClassName('close-modal')[0];
-var closeCardButton = document.getElementsByClassName('close-card')[0];
+var closeCardButton = document.getElementsByClassName('control-icons-item')[0];
 //Добавляем подробную информацию о карточке в модальное окно
 function createModalContent(modal, element) {
     if (element.getElementsByClassName('card-photo-item')[0]) {
@@ -86,6 +86,13 @@ function createModalContent(modal, element) {
         var descElem = element.getElementsByClassName('card-desc')[0];
         p.innerHTML = descElem.innerText;
         modal.appendChild(p);
+    }
+    if (element.getAttribute('data-priority')) {
+        var prior = document.createElement('p');
+        prior.className = 'priority-modal-content';
+        var priority = element.getAttribute('data-priority');
+        prior.innerHTML = priority + ' priority';
+        modal.appendChild(prior);
     }
 }
 //Массив объектов с информацией о радио-кнопках
@@ -121,17 +128,26 @@ function createRadioGroup(items) {
     return ul;
 }
 
-function getCheckedRadio(radio_group) {
-    for (var i = 0; i < radio_group.length; i++) {
-        var button = radio_group[i];
+function getCheckedRadio(radioGroup) {
+    for (var i = 0; i < radioGroup.length; i++) {
+        var button = radioGroup[i];
         if (button.checked) {
             return button;
         }
     }
     return undefined;
 }
+
+function setRadioChecked(radioButtons, priority) {
+    for (var i = 0; i < radioButtons.length; i++) {
+        console.log('adsc;');
+        if (radioButtons[i].getAttribute('data-priority') === priority) {
+            radioButtons[i].checked = 'true';
+        }
+    }
+}
 //Модальное окно с созданием новой карточки
-function createModalNewCard(modal, targetUl) {
+function workingWithModal(modal, type, targetElem) {
     var spanTitle = document.createElement('span');
     spanTitle.className = 'modal-label';
     spanTitle.innerText = 'Title';
@@ -144,37 +160,54 @@ function createModalNewCard(modal, targetUl) {
     var textarea = document.createElement('textarea');
     textarea.name = 'modal-desc';
     textarea.classList.add('modal-textarea', 'modal-textarea-desc');
-    var addButton = document.createElement('a');
-    addButton.href = '#';
-    addButton.classList.add('add-task-button', 'modal-create-button');
-    addButton.innerText = 'Create new card';
+    var modalButton = document.createElement('a');
+    modalButton.href = '#';
+    modalButton.classList.add('add-task-button', 'modal-create-button');
+    if (type === 'create') {
+        modalButton.innerText = 'Create new card';
+    }
     var spanPriority = document.createElement('span');
     spanPriority.className = 'modal-label';
     spanPriority.innerText = 'Priority';
     var radio = createRadioGroup(priorities);
+    var radioButtons = radio.getElementsByClassName('radio');
     modal.appendChild(spanPriority);
     modal.appendChild(radio);
     modal.appendChild(spanTitle);
     modal.appendChild(inputTitle);
     modal.appendChild(spanDesc);
     modal.appendChild(textarea);
-    modal.appendChild(addButton);
+    modal.appendChild(modalButton);
+    if (type === 'edit') {
+        modalButton.innerText = 'Save card';
+        inputTitle.value = targetElem.getElementsByClassName('card-title')[0].innerText;
+        textarea.value = targetElem.getElementsByClassName('card-desc')[0].innerText;
+        setRadioChecked(radioButtons, targetElem.getAttribute('data-priority'));
+    }
     var currentCheckedRadio = null;
-    radio.onchange = function (event) {
+    radio.onchange = function(event) {
         currentCheckedRadio = event.target.getAttribute('data-priority');
     };
-    addButton.onclick = function (event) {
-        event.preventDefault();
-        var newCard = createNewCard(modal.getElementsByClassName('modal-input-title')[0].value, modal.getElementsByClassName('modal-textarea-desc')[0].value, currentCheckedRadio, targetUl);
-        newCard.addEventListener('dragstart', startDragging, false);
-        newCard.addEventListener('dragend', stopDragging, false);
-        newCard.addEventListener('click', onClickCard, false);
-        newCard.addEventListener('mouseover', removeCard, false);
-        newCard.addEventListener('mouseout', hideCloseButton, false);
-        targetUl.appendChild(newCard);
+    modalButton.onclick = function(event) {
+        if (type === 'create') {
+            event.preventDefault();
+            var newCard = createNewCard(modal.getElementsByClassName('modal-input-title')[0].value, modal.getElementsByClassName('modal-textarea-desc')[0].value, currentCheckedRadio, targetElem);
+            newCard.addEventListener('dragstart', startDragging, false);
+            newCard.addEventListener('dragend', stopDragging, false);
+            newCard.addEventListener('click', onClickCard, false);
+            newCard.addEventListener('mouseover', showControls, false);
+            newCard.addEventListener('mouseout', hideControlsButton, false);
+            targetElem.appendChild(newCard);
+        }
+        if (type === 'edit') {
+            event.preventDefault();
+            targetElem.getElementsByClassName('card-title')[0].innerText = inputTitle.value;
+            targetElem.getElementsByClassName('card-desc')[0].innerText = textarea.value;
+            targetElem.setAttribute('data-priority', getCheckedRadio(radioButtons).getAttribute('data-priority'));
+        }
         this.parentElement.parentElement.classList.remove('active');
-        var content = this.parentElement.parentElement.getElementsByClassName('modal-content')[0];
-        content.innerHTML = '';
+        var contentModal = this.parentElement.parentElement.getElementsByClassName('modal-content')[0];
+        contentModal.innerHTML = '';
     };
 }
 //Обработка клика на карточку
@@ -184,56 +217,92 @@ function onClickCard(event) {
             modal.classList.add('active');
             createModalContent(modal.getElementsByClassName('modal-content')[0], this);
         }
+        console.log(this);
+        if (event.target.tagName === 'I' && event.target.classList.contains('ion-close-round')) {
+            removeCard(this);
+        }
+        if (event.target.tagName === 'I' && event.target.classList.contains('ion-edit')) {
+            editCard(this);
+        }
     }
 }
 //Закрываем модальное окно с подробной информацией о карточке
-closeModalButton.addEventListener('click', function () {
+closeModalButton.addEventListener('click', function() {
     modal.classList.remove('active');
     var content = modal.getElementsByClassName('modal-content')[0];
     content.innerHTML = '';
 });
+
+function showControls() {
+    var controlButtons = this.getElementsByClassName('control-icons')[0];
+    controlButtons.style.opacity = '1';
+}
 //Удаление карточки
-function removeCard() {
-    var closeButton = this.getElementsByClassName('close-card')[0];
-    closeButton.style.opacity = '1';
-    closeButton.onclick = function (event) {
-        var that = this;
-        var alert = createAlert('Are you sure?', 'Are you sure that you want to delete this card?');
-        alert.onclick = function (event) {
-            if (event.target.classList.contains('alert-button-ok')) {
-                this.parentNode.removeChild(this);
-                var currentCard = that.parentElement;
-                var ul = currentCard.parentElement;
-                ul.removeChild(currentCard);
-            }
-            if (event.target.classList.contains('alert-button-no')) {
-                this.parentNode.removeChild(this);
-            }
-        };
+function removeCard(currentThis) {
+    var closeButton = currentThis.getElementsByClassName('control-icons-item')[0];
+
+    var that = currentThis;
+    var alert = createAlert('Are you sure?', 'Are you sure that you want to delete this card?');
+    alert.onclick = function(event) {
+        if (event.target.classList.contains('alert-button-ok')) {
+            this.parentNode.removeChild(this);
+            var currentCard = that;
+            var ul = currentCard.parentElement;
+            ul.removeChild(currentCard);
+        }
+        if (event.target.classList.contains('alert-button-no')) {
+            this.parentNode.removeChild(this);
+        }
     };
 }
+
+function removeCard(currentThis) {
+    var closeButton = currentThis.getElementsByClassName('control-icons-item')[0];
+
+    var that = currentThis;
+    var alert = createAlert('Are you sure?', 'Are you sure that you want to delete this card?');
+    alert.onclick = function(event) {
+        if (event.target.classList.contains('alert-button-ok')) {
+            this.parentNode.removeChild(this);
+            var currentCard = that;
+            var ul = currentCard.parentElement;
+            ul.removeChild(currentCard);
+        }
+        if (event.target.classList.contains('alert-button-no')) {
+            this.parentNode.removeChild(this);
+        }
+    };
+}
+
+function editCard(currentThis) {
+    var modal = document.getElementsByClassName('modal')[0];
+    if (!modal.classList.contains('active')) {
+        modal.classList.add('active');
+        workingWithModal(modal.getElementsByClassName('modal-content')[0], 'edit', currentThis);
+    }
+}
 //Прячем кнопку удаления
-function hideCloseButton() {
-    this.getElementsByClassName('close-card')[0].style.opacity = '0';
+function hideControlsButton() {
+    this.getElementsByClassName('control-icons')[0].style.opacity = '0';
 }
 //Вешаем обработчики событий на карточки и колонки
 var cards = document.querySelectorAll('.card');
-[].forEach.call(cards, function (card) {
+[].forEach.call(cards, function(card) {
     card.addEventListener('dragstart', startDragging, false);
     card.addEventListener('dragend', stopDragging, false);
-    card.addEventListener('mouseover', removeCard, false);
-    card.addEventListener('mouseout', hideCloseButton, false);
+    card.addEventListener('mouseover', showControls, false);
+    card.addEventListener('mouseout', hideControlsButton, false);
     card.addEventListener('click', onClickCard, false);
 });
 var columns = document.querySelectorAll('.column');
-[].forEach.call(columns, function (column) {
+[].forEach.call(columns, function(column) {
     column.addEventListener('dragenter', dragInto, false);
     column.addEventListener('dragover', dragWithin, false);
     column.addEventListener('dragleave', dragOut, false);
     column.addEventListener('drop', drop, false);
 });
 //Добавление новой карточки
-function createNewCard(title, desc, priority, targetUl) {
+function createNewCard(title, desc, priority, targetElem) {
     var li = document.createElement('li');
     li.className = 'card';
     li.setAttribute("draggable", true);
@@ -255,27 +324,32 @@ function createNewCard(title, desc, priority, targetUl) {
     p.innerText = desc;
     cardInfo.appendChild(h3);
     cardInfo.appendChild(p);
-    var i = document.createElement('i');
-    i.classList.add('ionicons', 'ion-android-close', 'close-card');
-    var input = document.createElement('i');
+    var controlIcons = document.createElement('div');
+    controlIcons.className = 'control-icons';
+    var iClose = document.createElement('i');
+    iClose.classList.add('ionicons', 'ion-close-round', 'control-icons-item');
+    var iEdit = document.createElement('i');
+    iEdit.classList.add('ionicons', 'ion-edit', 'control-icons-item');
+    controlIcons.appendChild(iClose);
+    controlIcons.appendChild(iEdit);
     li.appendChild(cardPhoto);
     li.appendChild(cardInfo);
-    li.appendChild(i);
-    targetUl.appendChild(li);
+    li.appendChild(controlIcons);
+    targetElem.appendChild(li);
     return li;
 }
 //Обработка клика создания новой карточки
 function onClickNewCardButton(e) {
     e.preventDefault();
-    var targetUl = this.parentElement.getElementsByClassName('card-list')[0];
+    var targetElem = this.parentElement.getElementsByClassName('card-list')[0];
     if (!modal.classList.contains('active')) {
         modal.classList.add('active');
-        createModalNewCard(modal.getElementsByClassName('modal-content')[0], targetUl);
+        workingWithModal(modal.getElementsByClassName('modal-content')[0], 'create', targetElem);
     }
 }
 //Вешаем событие на кнопку добавление карточки
 var newCardButtons = document.getElementsByClassName('add-task-button');
-[].forEach.call(newCardButtons, function (newCardButton) {
+[].forEach.call(newCardButtons, function(newCardButton) {
     newCardButton.addEventListener('click', onClickNewCardButton, false);
 });
 //Создание уведомлений
